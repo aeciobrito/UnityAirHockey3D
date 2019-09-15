0e8fb94b5c204976c0c47adc549dc6df13a7137d
Aecio Desktop <aeciobrito@gmail.com>
2019-09-15 10:35:58 -0300

    Paddle movement and corner corrections in the filed

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    private Rigidbody _rigidBody;
+
+    private Vector3 _movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidBody.velocity = _movement * _speed;
+    }
+}
