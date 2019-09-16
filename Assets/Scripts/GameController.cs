b7ada3379e063f6a019515608c52a68cf842e394
Aecio Desktop <aeciobrito@gmail.com>
2019-09-16 15:58:56 -0300

    Game controller singleton

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    #region singleton
+    private static GameController _instance;
+
+    public static GameController Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                GameObject gameObject = new GameObject("GameController");
+                gameObject.AddComponent<GameController>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    #endregion
+
+    private int[] scorePoints = new int[2];
+
+    public void Goal(int whoScored)
+    {
+        scorePoints[whoScored]++;
+    }
+}
---
+++ Assets/Scripts/Puck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puck : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            GameController.Instance.Goal(transform.position.z > 0 ? 0 : 1);
+        }
+    }
+}
