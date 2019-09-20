8ef034b04a26b3ea2f0c9a4c89a9e832295a8aa2
Aecio Desktop <aeciobrito@gmail.com>
2019-09-19 22:09:31 -0300

    Win condition and restart game.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class GameController : MonoBehaviour
 {
     #region singleton
@@ -28,14 +30,18 @@ public class GameController : MonoBehaviour
     #endregion
 
     [SerializeField] GameObject _player0, _player1, _puck;
-    [SerializeField] Text _player0Score, _player1Score;
+    [SerializeField] Text _player0Score, _player1Score, _gammeWinner;
 
     private int[] scorePoints = new int[2];
     float startPosition = 1.2f;
 
     private void Start()
     {
+        Time.timeScale = 1;
         UpdateScore();
+        _gammeWinner.gameObject.SetActive(false);
+        _player0Score.gameObject.SetActive(true);
+        _player1Score.gameObject.SetActive(true);
     }
 
     public void Goal(int whoScored)
@@ -43,6 +49,9 @@ public class GameController : MonoBehaviour
         scorePoints[whoScored]++;
         ResetPositions(whoScored);
         UpdateScore();
+
+        if (scorePoints[0] >= 3 || scorePoints[1] >= 3)
+            GameOver(whoScored);
     }
 
     public void ResetPositions(int whoIsTheTurn)
@@ -55,12 +64,26 @@ public class GameController : MonoBehaviour
 
         _puck.GetComponent<Rigidbody>().velocity = Vector3.zero;
         _puck.gameObject.transform.position = new Vector3(0, 0, startPosition * (whoIsTheTurn == 0 ? 1 : -1));
-
     }
 
     public void UpdateScore()
     {
-        _player0Score.text = "SCORE: " + scorePoints[0];
-        _player1Score.text = "SCORE: " + scorePoints[1];
+        _player0Score.text = "SCORE1: " + scorePoints[0];
+        _player1Score.text = "SCORE2: " + scorePoints[1];        
+    }
+
+    public void GameOver(int whoWin)
+    {
+        Time.timeScale = 0;
+        _gammeWinner.gameObject.SetActive(true);
+        _player0Score.gameObject.SetActive(false);
+        _player1Score.gameObject.SetActive(false);
+        _gammeWinner.text = "PLAYER " + (whoWin + 1) + "\n WINS!";
+        
+    }
+
+    public void GameRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
